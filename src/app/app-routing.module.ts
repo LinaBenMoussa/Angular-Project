68713fb29273285e2852c6cd688537aee9f93015
lina <benmossalina@iit.ens.tn>
2024-04-06 10:57:40 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelsComponent } from './hotels/hotels.component';
+import { HomeComponent } from './home/home.component';
+import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    component : HomeComponent  }
+  ,{
+    path:'hotels',
+    pathMatch:'full',
+    component : HotelsComponent
+
+  },
+  {
+    path:'hotelDetails',
+    pathMatch:'full',
+    component : HotelDetailsComponent
+
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
