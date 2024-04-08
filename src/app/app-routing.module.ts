56a62b15798d71f38a1f5b6d92a2c766523365a3
lina <benmossalina@iit.ens.tn>
2024-04-09 00:41:05 +0100

    supprimer main.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,37 +4,14 @@ import { HotelsComponent } from './hotels/hotels.component';
 import { HomeComponent } from './home/home.component';
 import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'  // Redirige '/' vers '/home'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-<<<<<<< HEAD
-    path: 'hotels',
-    component: HotelsComponent
-  },
-  {
-    path: 'hotelDetails',
-    component: HotelDetailsComponent
-  },
-  {
-    path: '**',  // Route wildcard pour les URL non reconnues
-    redirectTo: 'home'  // Redirige toutes les autres URL vers '/home'
-  }
-=======
-    path:'dashboard',
-   
-    component : DashboardComponent
 
-  },
-  
->>>>>>> 4b82fa5a033caeef93d4ebec25cd34c7734f31f2
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'hotels', component: HotelsComponent },
+  { path: 'hotelDetails', component: HotelDetailsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'home' } // Route wildcard pour les URL non reconnues
 ];
 
 @NgModule({
