77101196d21ef7fd4bfc9b5233391d3227e0b0c8
lina <benmossalina@iit.ens.tn>
2024-04-15 00:19:23 +0100

    hotel booking

--- src/app/hotel-booking/hotel-booking.component.ts
+++ src/app/hotel-booking/hotel-booking.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any; // Déclaration pour utiliser jQuery dans un composant Angular
 
 @Component({
   selector: 'app-hotel-booking',
   templateUrl: './hotel-booking.component.html',
   styleUrls: ['./hotel-booking.component.css']
 })
-export class HotelBookingComponent {
+export class HotelBookingComponent implements OnInit {
+  currentStep: number = 0;
+  tabcount: number = 0;
 
+  ngOnInit(): void {
+    $('#donation_next').on('click', (e: Event) => this.onNextClick(e));
+    $('#donation_back').on('click', (e: Event) => this.onBackPressed(e));
+  }
+
+  onNextClick(event: Event): void {
+    event.preventDefault();
+
+    this.currentStep++;
+    $('.current-step').html(this.currentStep.toString());
+
+    $(event.target).parents('.booking-form-block').next().addClass('active');
+    $(event.target).parents('.booking-form-block').removeClass('active');
+
+    this.tabcount += 1;
+  }
+
+  onBackPressed(event: Event): void {
+    event.preventDefault();
+
+    this.currentStep--;
+    $('.current-step').html(this.currentStep.toString());
+
+    $(event.target).parents('.booking-form-block').prev().addClass('active');
+    $(event.target).parents('.booking-form-block').removeClass('active');
+
+    this.tabcount -= 1;
+  }
 }
